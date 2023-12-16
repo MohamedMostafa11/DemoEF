3b406de43c80f6d6daa30f20e9e618ebed3b367e
Mohamed Mostafa <@MohamedMostafa11>
2023-12-16 20:21:02 +0200

    End Create Entities

--- DemoEF/Models/Student.cs
+++ DemoEF/Models/Student.cs
@@ -1,8 +1,15 @@
-﻿namespace DemoEF.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DemoEF.Models
 {
     public class Student
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int studentId { get; set; }
+        [MinLength(5)]
+        [MaxLength(50)]
         public string studentName { get; set; }
         public bool IsActive { get; set; }
     }
--- DemoEF/Models/Teacher.cs
+++ DemoEF/Models/Teacher.cs
@@ -1,9 +1,17 @@
-﻿namespace DemoEF.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DemoEF.Models
 {
     public class Teacher
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int teacherId { get; set; }
+        [MinLength(10)]
+        [MaxLength(60)]
         public string teacherName { get; set; }
+        [Range(23,70)]
         public int teacherage { get; set; }
     }
 }
