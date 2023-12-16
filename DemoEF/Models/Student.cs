036ae6c01c328ff9de6b4fae08c50a8701910fe8
Mohamed Mostafa <@MohamedMostafa11>
2023-12-16 20:11:59 +0200

    Add project files.

---
+++ DemoEF/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoEF.Models
+{
+    public class Student
+    {
+        public int studentId { get; set; }
+        public string studentName { get; set; }
+        public string IsActive { get; set; }
+    }
+}
---
+++ DemoEF/Models/Teacher.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoEF.Models
+{
+    public class Teacher
+    {
+        public int teacherId { get; set; }
+        public string teacherName { get; set; }
+        public int teacherage { get; set; }
+    }
+}
