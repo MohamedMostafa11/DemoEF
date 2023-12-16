86b2a0ba75800346b03481b224c140a6b4e782d2
Mohamed Mostafa <@MohamedMostafa11>
2023-12-16 21:06:28 +0200

    End Create migra

---
+++ DemoEF/Context/MyContext.cs
@@ -0,0 +1,32 @@
+﻿using DemoEF.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DemoEF.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Student { get; set; }
+        public DbSet<Teacher> Teacher { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                IConfigurationRoot configurationRoot = builder.Build();
+                var connectionString = configurationRoot.GetConnectionString("MyDatabaseConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
